4ce1ae297e3133666a11860f73848289b8ea3c8e
iamdev-james <jamesoluwabukola87@gmail.com>
2022-02-04 15:14:13 -0100

    finishing touch

--- wiping.js
+++ wiping.js
@@ -1,24 +1,64 @@
+let email;
+
+// CHECKING FOR THE VALID EMAIL
+function valEmail() {
+  let emailRecieved_M = document.querySelector('#email_M').value;
+  let emailRecieved_D = document.querySelector('#email_D').value;
+  if (emailRecieved_D){
+    email = emailRecieved_D;
+  } else {
+    email = emailRecieved_M;
+  }
+}
+
+// RESPONSE VISUALIZATION
+function resVil(responseRecieved) {
+  if (responseRecieved == 'Email added') {
+    Swal.fire({
+      icon: 'success',
+      title: 'Email Recieved Successfully',
+      text: 'You will recieved a mail from us soon'
+    })
+    email ='';
+  } else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: responseRecieved + '!'
+    })    
+  }
+}
+
+// SBMITTING MAIL TO SERVER
 function submitMail (e) {
   e.preventDefault();
-  let emailRecieved = document.querySelector('#email').value;
+  valEmail();
+  let maildata = { email: email };
   
-  let maildata = { email: emailRecieved };
-  
-  console.log(emailRecieved)
-//   var myHeaders = new Headers();
-//   myHeaders.append("Content-Type", "application/json");
+  Swal.fire({
+    icon: 'info',
+    title: 'Please wait while we save your Address',
+    showConfirmButton: false
+  })
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-//   var raw = JSON.stringify( maildata );
+  var raw = JSON.stringify( maildata );
 
-//   var requestOptions = {
-//   method: 'POST',
-//   headers: myHeaders,
-//   body: raw,
-//   redirect: 'follow'
-//   };
+  var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+  };
 
-//  fetch("http://localhost:4400/add", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
+ fetch("https://wiping-waiting-list.herokuapp.com/add", requestOptions)
+  .then(response => response.text())
+  .then(result => resVil(JSON.parse(result).message))
+  .catch(error => 
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'Try to fill in your details again'
+    }));
 }
