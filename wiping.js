e8c8932ce718fb08f595bd9291ae98404ab8d392
iamdev-james <jamesoluwabukola87@gmail.com>
2022-02-09 19:09:46 -0100

    Wiping completed

--- wiping.js
+++ wiping.js
@@ -11,54 +11,8 @@ function valEmail() {
   }
 }
 
-// RESPONSE VISUALIZATION
-function resVil(responseRecieved) {
-  if (responseRecieved == 'Email added') {
-    Swal.fire({
-      icon: 'success',
-      title: 'Email Recieved Successfully',
-      text: 'You will recieved a mail from us soon'
-    })
-    email ='';
-  } else {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: responseRecieved + '!'
-    })    
-  }
-}
-
 // SBMITTING MAIL TO SERVER
 function submitMail (e) {
   e.preventDefault();
   valEmail();
-  let maildata = { email: email };
-  
-  Swal.fire({
-    icon: 'info',
-    title: 'Please wait while we save your Address',
-    showConfirmButton: false
-  })
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  var raw = JSON.stringify( maildata );
-
-  var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-  };
-
- fetch("https://wiping-waiting-list.herokuapp.com/add", requestOptions)
-  .then(response => response.text())
-  .then(result => resVil(JSON.parse(result).message))
-  .catch(error => 
-    Swal.fire({
-      icon: 'error',
-      title: 'Error',
-      text: 'Try to fill in your details again'
-    }));
 }
